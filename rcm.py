b26bea0e145d314ae0a8c48d9f53a349fb08e37b
AntonAlonzo <87627127+AntonAlonzo@users.noreply.github.com>
2024-04-04 11:34:35 +0800

    initial code

---
+++ rcm.py
@@ -0,0 +1,44 @@
+import os
+import numpy as np
+import opencv as cv2
+
+# "Constant" declaration of Robinsons Compass Mask kernels
+RCM = [
+    np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]),  # East
+    np.array([[0, 1, 2], [-1, 0, 1], [-2, -1, 0]]),  # South East
+    np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]),  # South
+    np.array([[2, 1, 0], [1, 0, -1], [0, -1, -2]]),  # South West
+    np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]]),  # West
+    np.array([[0, -1, -2], [1, 0, -1], [2, 1, 0]]),  # North West
+    np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]]),  # North
+    np.array([[-2, -1, 0], [-1, 0, 1], [0, 1, 2]])   # North East
+]
+
+# Function to create a folder to hold the different compass outputs for a single image input
+def createDirectory(dataName):
+
+    path = "./compasses/" + dataName
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+        print("Folder for %s data created" % path)
+    else:
+        print("Folder for %s data already exists" % path)
+
+def applyMasks(img, RCM=RCM):
+    gradient_images = [cv2.filter2D(img, -1, mask) for mask in RCM]
+    absolute_gradients = [np.abs(gradient) for gradient in gradient_images]
+
+    # TODO: use createDirectory to make folder to store all compass data of the img
+
+    return absolute_gradients
+    
+
+def detectEdge(img):
+
+    abs_gradients = applyMasks(img)
+    img_edged = np.sum(abs_gradients, axis = 0)
+
+    # TODO: write image to final folder
+    
+    return img_edged 
